fb7fad3d8cac004986afc7a34da2b8bf90478a9a
Courtney Bean <calysabean@gmail.com>
2019-01-30 00:35:16 -0500

    travis

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/capstone-db';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/capstone-db';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/goals-app';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/goals-app-test';
 
 exports.PORT = process.env.PORT || 8080;
 
