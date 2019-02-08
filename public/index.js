8f4e1de915d8eb1e4c44b079a114af5cb53f0ae8
Courtney Bean <calysabean@gmail.com>
2019-02-07 21:40:58 -0500

    js additions

--- public/index.js
+++ public/index.js
@@ -1,31 +1,31 @@
 
 
 function nextPage() {
-  $('.submit').submit(function (event) {
-    event.preventDefault();
-	$('.results').load('index.html')
-})
+    $('.submit').submit(function (event) {
+        event.preventDefault();
+        $('.results').load('index.html')
+    })
 }
 
 function getAllGoals() {
-	$('.dashboaresults1').html("");
-	$.ajax({
-			method: 'GET',
-			url: '/goals',
-			dataType: 'json'
-		})
-		.done((goals) => {
+    $('.dashboaresults1').html("");
+    $.ajax({
+        method: 'GET',
+        url: '/goals',
+        dataType: 'json'
+    })
+        .done((goals) => {
             console.log(goals);
-			goals.forEach((option) => {
-            $('.dashboaresults1').append(`
+            goals.forEach((option) => {
+                $('.dashboaresults1').append(`
             
 
         <div class="viewPort formCss">
         <section class="formCss3">
         <form action="pick-goals.html" class="finalResults">
           <fieldset>
-          <label for="answerOption">
-          <input title="checkBox" id="answerOption" type="checkbox" value="${option.goal}" name="answer" >
+          <label for="${option._id}">
+          <input title="checkBox" id="${option._id}" class="answerOption1" type="checkbox" value="${option.goal}" name="answer" >
           </label>
           </fieldset>
           <p>${option.category}</p>
@@ -34,13 +34,13 @@ function getAllGoals() {
           </form>
         </div> 
 			`)
-			})
-		})
-} 
+            })
+        })
+}
 
 
 
-$('.jSsignUpForm').on('click', '.jSsignUP1', function(event) {
+$('.jSsignUpForm').on('click', '.jSsignUP1', function (event) {
     event.preventDefault();
     let registeredUser = {};
     registeredUser.username = $('.jsUserName').val();
@@ -55,42 +55,42 @@ $('.jSsignUpForm').on('click', '.jSsignUP1', function(event) {
 });
 
 function processUser(registeredUser) {
-    fetch('/api/users', 
-    {
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        method: "POST",
-        body: JSON.stringify(registeredUser)
-    })
-    .then(response => {
-        if (response.status === 201) {
-            window.location.href = "pick-goals.html";  
-        }
-        else {   
-           return response.json()
-        }
-        
-    })
-    .then(response => {
-      $('.jSsignUP1').text(response.location +":   "+ response.message);
-    })
-    .catch(error => console.log('Oops something went wrong'));
+    fetch('/api/users',
+        {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: "POST",
+            body: JSON.stringify(registeredUser)
+        })
+        .then(response => {
+            if (response.status === 201) {
+                window.location.href = "pick-goals.html";
+            }
+            else {
+                return response.json()
+            }
+
+        })
+        .then(response => {
+            $('.jSsignUP1').text(response.location + ":   " + response.message);
+        })
+        .catch(error => console.log('Oops something went wrong'));
 };
 
-   $(() => {
+$(() => {
     getAllGoals();
-    nextPage(); 
-  });
+    nextPage();
+});
 
- /* const answerOption = document.getElementById("answerOption");
+/* const answerOption = document.getElementById("answerOption");
 
-  answerOption.addEventListener("submit", (e) => {
-      e.preventDefault();
+ answerOption.addEventListener("submit", (e) => {
+     e.preventDefault();
 
-      console.log("form has been submitted")
-  })*/
+     console.log("form has been submitted")
+ })*/
 
 /*$(document).ready(function() {
 $('.pickGoals').on('click', function(){
@@ -109,97 +109,97 @@ event.preventDefault();
 return document.html(`Are you sure you want to return to the options page?`    
 });
       use modal?*/
+
+/* function for pick-goals page*/
+
+/*function selectedData(){
+  $('.pickGoals').on('click', function (event) {
+    event.preventDefault();
  
-  /* function for pick-goals page*/
-
-  /*function selectedData(){
-    $('.pickGoals').on('click', function (event) {
-      event.preventDefault();
-  
-      $.each($("input[name='answer'](:checked)"), function() {
+    $.each($("input[name='answer'](:checked)"), function() {
 for (let i = 0; i < )
-    });
-      }) */
+  });
+    }) */
 
-     /* function displayResult(result) {
-        return `
-        <div class="viewPort formCss">
-        <section class="formCss3">
-        <form class="finalResults">
-          <fieldset>
-          <label for="answerOption">
-          <input title="checkBox" class="answerOption" type="checkbox" value="${result.goal}" name="answer" >
-          </label>
-          </fieldset>
-          </form>
-          <p>${result.category}</p>
-          <p>${result.goal}</p>
-          </section>
-        </div> 
-        `;
-      }*/
+/* function displayResult(result) {
+   return `
+   <div class="viewPort formCss">
+   <section class="formCss3">
+   <form class="finalResults">
+     <fieldset>
+     <label for="answerOption">
+     <input title="checkBox" class="answerOption" type="checkbox" value="${result.goal}" name="answer" >
+     </label>
+     </fieldset>
+     </form>
+     <p>${result.category}</p>
+     <p>${result.goal}</p>
+     </section>
+   </div> 
+   `;
+ }*/
 
 
- /*function getAllGoals1() {
-	$('.my-chosen-results').html("");
-	$.ajax({
-			method: 'POST',
-			url: '/my-vision',
-			dataType: 'json'
-		})
-		.done((goals) => {
-            console.log(goals);
-			goals.forEach((option) => {
-                $('.my-chosen-results').append(`
-        <div action="my-vision.html" method="POST" class="viewPort formCss">
-        <section class="formCss3">
-        <form action="pick-goals.html" class="finalResults">
-          <fieldset>
-          <label for="answerOption">
-          <input title="checkBox" id="answerOption" class="theClass" type="checkbox" value="${option.goal}" name="answer" >
-          </label>
-          </fieldset>
-          <p>${option.category}</p>
-          <p>${option.goal}</p>
-          </section>
-          </form>
-        </div> 
-			`)
+/*function getAllGoals1() {
+   $('.my-chosen-results').html("");
+   $.ajax({
+           method: 'POST',
+           url: '/my-vision',
+           dataType: 'json'
+       })
+       .done((goals) => {
+           console.log(goals);
+           goals.forEach((option) => {
+               $('.my-chosen-results').append(`
+       <div action="my-vision.html" method="POST" class="viewPort formCss">
+       <section class="formCss3">
+       <form action="pick-goals.html" class="finalResults">
+         <fieldset>
+         <label for="answerOption">
+         <input title="checkBox" id="answerOption" class="theClass" type="checkbox" value="${option.goal}" name="answer" >
+         </label>
+         </fieldset>
+         <p>${option.category}</p>
+         <p>${option.goal}</p>
+         </section>
+         </form>
+       </div> 
+           `)
+                   })
+   }
+   */
+
+function getAllGoals1() {
+    $('.my-chosen-results').html("");
+    $.ajax({
+        method: 'POST',
+        url: '/my-vision',
+        dataType: 'json'
+    })
+    .done((goals) => {
+        console.log(goals);
+        goals.forEach((option) => {
+            for (let i = 0; i < option.length; i++) {
+                if ($('.answerOption1:checkbox:checked')) {
+                    $('.answerOption1:checkbox:checked').map(function () {
+                        $('.my-chosen-results').append(`
+                            <div class="viewPort formCss">
+                            <section class="formCss3">
+                            <form action="pick-goals.html" class="finalResults">
+                            <fieldset>
+                            <label for="answerOption">
+                            <input title="checkBox" id="answerOption" class="theClass" type="checkbox" value="${option.goal}" name="answer" >
+                            </label>
+                            </fieldset>
+                            <p>${option.category}</p>
+                            <p>${option.goal}</p>
+                            </section>
+                            </form>
+                            </div> 
+                        `)
                     })
-    }
-    */
-
-    function getAllGoals1() {
-        $('.my-chosen-results').html("");
-        $.ajax({
-                method: 'POST',
-                url: '/my-vision',
-                dataType: 'json'
-            })
-            .done((goals) => {
-                console.log(goals);
-                goals.forEach((option) => {
-                    for (let i = 0; i < option.length; i++) {
-                        if ($('.theClass:checkbox:checked').map(function() {
-                    $('.my-chosen-results').append(`
-            <div class="viewPort formCss">
-            <section class="formCss3">
-            <form action="pick-goals.html" class="finalResults">
-              <fieldset>
-              <label for="answerOption">
-              <input title="checkBox" id="answerOption" class="theClass" type="checkbox" value="${option.goal}" name="answer" >
-              </label>
-              </fieldset>
-              <p>${option.category}</p>
-              <p>${option.goal}</p>
-              </section>
-              </form>
-            </div> 
-                `)
-                        })
-                        )
-        })
-        })
+                }
             }
-
-            
+        })
+    })
+}
