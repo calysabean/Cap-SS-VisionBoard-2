47bcc6df933210c36443004b69a4f3f158fb6b6d
Courtney Bean <calysabean@gmail.com>
2019-03-20 08:24:20 -0400

    testing js file

--- test/test-goals.js
+++ test/test-goals.js
@@ -1,17 +1,46 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const faker = require('faker');
+const mongoose = require('mongoose');
 
+//const should = chai.should();
+
+//const { GoalPost } = require('../models');
 const {app, runServer, closeServer} = require('../server');
+//const { TEST_DATABASE_URL } = require('../config');
 
-const { TEST_DATABASE_URL } = require('../config');
-
-
-const should = chai.should();
-
+const expect = chai.expect;
 
 chai.use(chaiHttp);
 
 describe('Goals', function() {
+
+  before(function() {
+    return runServer();
+  });
+
+ 
+  after(function() {
+    return closeServer();
+  });
+  
+  /*
+  it('should list goals on GET', function() {
+    return chai
+    .request(app)
+    .get("/api/goals/posts")
+    .then(function(res) {
+      expect(res).to.have.status(200);
+      expect(res).to.be.json;
+      expect(res.body).to.be.a("object");
+
+     
+    });
+});
+})*/
+
+
+/*describe('Goals', function() {
 
   
   before(function() {
@@ -21,12 +50,30 @@ describe('Goals', function() {
  
   after(function() {
     return closeServer();
-  });
+  });*/
 
- /*it('should list items on GET', () => {
+
+  it('should add an item on POST', function() {
+    const newItem = {category: 'Career', comments: 'String'};
+     return chai.request(app)
+       .post('/api/goals/posts')
+       .send(newItem)
+       .then(function(res) {
+         res.should.have.status(201);
+         res.should.be.json;
+         res.body.should.be.a('object');
+         res.body.should.include.keys('category', 'comments');
+         res.body.id.should.not.be.null;
+         
+         
+         res.body.should.deep.equal(Object.assign(newItem, {id: res.body.id}));
+       });
+   });
+
+ it('should list items on GET', () => {
    
     return chai.request(app)
-      .get('/goals')
+      .get('/api/goals')
       .then( res => {
         res.should.have.status(200);
         res.should.be.json;
@@ -38,38 +85,20 @@ describe('Goals', function() {
         item.should.include.keys(expectedKeys);
         });
       });
-  });*/
-
-  
-  it('should add an item on POST', function() {
-   const newItem = {category: 'a', comments: [], goal: 'WIN'};
-    return chai.request(app)
-      .post('/goals')
-      .send(newItem)
-      .then(function(res) {
-        res.should.have.status(201);
-        res.should.be.json;
-        res.body.should.be.a('object');
-        res.body.should.include.keys('category', 'goal');
-        res.body.id.should.not.be.null;
-        
-        
-        res.body.should.deep.equal(Object.assign(newItem, {id: res.body.id}));
-      });
-  });
-
+  }); 
+})
+  /*
 
   it('should update items on PUT', function() {
     
     const updateData = {
       category: 'Yes',
-      goal: 'YES',
-      checked: true
+      comments: 'YES'
     };
 
     return chai.request(app)
 
-     .get('/goals')
+     .get('/api/goals/posts')
       .then(function(res) {
         updateData.id = res.body[0].id;
        
@@ -86,7 +115,7 @@ describe('Goals', function() {
   it('should delete items on DELETE', function() {
     return chai.request(app)
 
-      .get('/goals')
+      .get('/api/goals/posts')
       .then(function(res) {
         return chai.request(app)
           .delete(`/goals/${res.body[0].id}`);
@@ -94,5 +123,5 @@ describe('Goals', function() {
       .then(function(res) {
         res.should.have.status(204);
       });
-  });
-});
+  });*/
+//});
